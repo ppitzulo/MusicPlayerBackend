3e5b9cc219324c15e5942fd199fae860704b61e9
Peter Pitzulo <plpitzulo@protonmail.com>
2023-06-01 17:05:28 -0400

    Fix runtime formatting

--- audiomanager/models.py
+++ audiomanager/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from datetime import timedelta
+from django.conf import settings
+import datetime
 import eyed3
 import os
-from django.conf import settings
 
 # Create your models here.
 class AudioFile(models.Model):
     audio_file = models.FileField(upload_to='audio/')
     title = models.CharField(max_length=255, default="Untitled")
     thumbnail = models.ImageField(upload_to="thumbnails/", blank=True, null=True)
-    runtime = models.DurationField(default=timedelta(seconds=0))
+    runtime = models.CharField(max_length=10, default="0:00")
     artist = models.CharField(max_length=255, default="Unknown")
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
@@ -19,18 +20,14 @@ class AudioFile(models.Model):
             self.title = self.audio_file.name.split('.')[0]
         super().save(*args, **kwargs)
         
+        # Get the file path
         file_path = self.audio_file.path
         audio = eyed3.load(file_path)
 
         if audio.info is not None:
-            # File has been uploaded and the model instance is created
-
-            # Get the file path
-
             # Extract metadata from the audio file using the file path
-            duration_seconds = int(audio.info.time_secs)
 
-            self.runtime = timedelta(seconds=duration_seconds)
+            self.runtime = self.format_runtime(int(audio.info.time_secs))
             if audio.tag.artist is not None:
                 self.artist = audio.tag.artist
 
@@ -39,6 +36,18 @@ class AudioFile(models.Model):
 
             # Save the model instance with the extracted metadata
             super().save(*args, **kwargs)
+            
+    def format_runtime(self, runtime):
+        # Takes runtime in seconds and converts to H:M:S or M:S if less than an hour
+        
+        hours, remainder = divmod(runtime, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        print(minutes)
+
+        if hours > 0:
+            return f'{hours}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}'
+        else:
+            return f'{minutes}:{str(seconds).zfill(2)}'
 
 def extract_thumbnail(audio_file_path):
     audio = eyed3.load(audio_file_path)
@@ -57,3 +66,4 @@ def extract_thumbnail(audio_file_path):
 
             # Exit the loop after finding the first thumbnail
             return thumbnail_url
+
